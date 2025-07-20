106c4a7c6bdef319bf2a3907c051e18d0288dd6d
Flannel <nat.stott@gmail.com>
2025-07-20 16:52:13 +0100

    using multimesh buffer directly

--- CodeMultimMesh.gd
+++ CodeMultimMesh.gd
@@ -5,10 +5,10 @@ extends Marker3D
 @export var testlayermaterial :Material
 var testlayernum=0.0
 var rd: RenderingDevice
-var multimeshid :RID
+#var multimeshid :RID
 var compute_list
 var pipeline
-var planet_Multimesh
+var planet_Multimesh :RID
 var meshbuffer :RID
 var firstrun :bool
 var tempbuffer
@@ -23,6 +23,7 @@ var DeltaTime=0.001
 var solarmass=50000.0
 var num_planets=15
 var paused=false
+var firstframe = true
 
 
 
@@ -36,19 +37,21 @@ func _ready():
 	var meshmaterial=TestMesh.get_active_material( 0)
 	meshmaterial.set_shader_parameter("Texture2DArrayParameter", texturearray)
 	meshmaterial.set_shader_parameter("Total_Layers", num_planets*1.0)
-	tempbuffer = maketestbuffer() #starting values
-	print("v3: ", velocities[3])
-	planet_Multimesh=CreateMultimesh(meshcount)
-	makeComputeShader()
-	tempbuffer.clear() #free memory
-	testlayermaterial.set_shader_parameter("Texture2DArrayParameter", texturearray)
-
 	
+	#print("v3: ", velocities[3])
+	planet_Multimesh=CreateMultimesh(meshcount)
+	maketestbuffer() #starting values
+	makeComputeShader()
+	#tempbuffer.clear() #free memory
+	testlayermaterial.set_shader_parameter("Texture2DArrayParameter", texturearray)
+	print("pipeline ready:" ,rd.compute_pipeline_is_valid(pipeline))
+	print("verletpipeline ready:" ,rd.compute_pipeline_is_valid(verletpipeline))	
+
 
 
 func _process(delta):
 	# Create a compute pipeline
-	if(!paused):
+	if(!paused and !firstframe):
 		var dispatchnumber=meshcount-1 # not updating sun
 		compute_list = rd.compute_list_begin()
 		rd.compute_list_bind_compute_pipeline(compute_list, pipeline)
@@ -60,13 +63,9 @@ func _process(delta):
 		rd.compute_list_bind_uniform_set(compute_list, uniform_set, 0)
 		rd.compute_list_dispatch(compute_list,dispatchnumber/32+1 , 1, 1)
 		rd.compute_list_end()
+	else : firstframe=false # planet data doesnt seem to be initialised on first frame
+	
 	if Input.is_action_just_pressed("Pause"): paused =!paused
-	
-	if Input.is_action_just_pressed("Flipcolour"):
-		testlayermaterial.set_shader_parameter("Layer", testlayernum)
-		testlayernum = (testlayernum+.10)-int(testlayernum/num_planets)*num_planets
-		
-	
 	
 
 
@@ -98,7 +97,7 @@ func makeComputeShader():
 	velocityuniform.add_id(velocitybuffer)
 	
 	#uint planetcount;  #uint dummy2;  #uint dummy3;  #uint dummy4;
-	#float BigG;  #float DummyMass;  #float DeltaTime;  #float packing;
+	#float BigG;  #float DummyMass;  #float DeltaTime;  #float packing(so 4floats);
 	var paramuniform := RDUniform.new()
 	paramuniform.uniform_type = RenderingDevice.UNIFORM_TYPE_UNIFORM_BUFFER
 	paramuniform.binding =1
@@ -118,60 +117,49 @@ func makeComputeShader():
 func maketestbuffer():
 	var interimarray=[]
 	var suns=1
+	var planetindex=0
 	if(suns>0):
 		#Make the Suns
-		
 		var temppos = Transform3D.IDENTITY.scaled(Vector3(3,3,3))
-		var tempbasis = temppos.basis
-		# values laid out to match multimesh array
-		interimarray.append_array([tempbasis.x.x, tempbasis.y.x, tempbasis.z.x, temppos.origin.x, \
-	tempbasis.x.y, tempbasis.y.y, tempbasis.z.y, temppos.origin.y, \
-	tempbasis.x.z, tempbasis.y.z, tempbasis.z.z, temppos.origin.z])
-		#Velocity buffer - 1:3 velocity, 4-mass 5:7 acceleration, 8 free
-		velocities.append_array([0.0, 0.0, 0.0, solarmass]); #Vx,Vy,Vz, M
-		velocities.append_array([0.0,0.0,0.0,0.0]) #zero acceleration
+		makeplanet(temppos,[0.0,0.0,0.0,solarmass],planetindex)
 	
 	var totalplanetmass=0
 	for i in range (1,meshcount):
 		var circrand=randf_range(-PI,PI)
 		var massrand=randf_range(.3,0.5)
-		var radiusrand=randf_range(20.,34.) 
-		var planetvel=sqrt(BigG*solarmass/radiusrand) # assume orbital velocity
+		var radiusrand=randf_range(20.,34.) #asteroid belt
 		if (i<num_planets):
 			radiusrand=3+2**i
 			massrand=randf_range(50,200)
-			print("Planet R, Period: ", radiusrand,";", 2*PI*radiusrand/planetvel)
-			
+
+		var planetvel=sqrt(BigG*solarmass/radiusrand) # assume orbital velocity
 		totalplanetmass+=massrand
 		var size=sqrt(massrand)*0.1
 		
-		#if (i==(meshcount/5)): planetvel=0; #testing collisions
 		var temppos = Transform3D.IDENTITY.scaled(Vector3(size,size,size))
-		#temppos = temppos.translated(Vector3((6+i/5000.0)*cos(circrand), randf_range(-1.0,1.0), (6+i/5000.0)*sin(circrand)))
 		temppos = temppos.translated(Vector3(radiusrand*cos(circrand), randf_range(-.10,.10), radiusrand*sin(circrand)))
-		var tempbasis = temppos.basis
-		var custom :Vector4 = Vector4(0,0,0,0)
-		var colour :Vector4 = Vector4(0,0,0,0)
-		# values laid out to match multimesh array
-		interimarray.append_array([tempbasis.x.x, tempbasis.y.x, tempbasis.z.x, temppos.origin.x, \
-	tempbasis.x.y, tempbasis.y.y, tempbasis.z.y, temppos.origin.y, \
-	tempbasis.x.z, tempbasis.y.z, tempbasis.z.z, temppos.origin.z ])#, \
-	#colour, custom.x])
+		var planetvelocity =[-planetvel*sin(circrand),0.0, planetvel*cos(circrand),massrand]
+		#velocities.append_array([0.0,0.0,0.0,0.0]) #zero acceleration
+		planetindex+=1
+		makeplanet(temppos,planetvelocity,planetindex)
+		print("index: ",planetindex)
 	
-		# Array 1:3 velocity, 4-mass 5:7 acceleration, 8 free
-		velocities.append_array([-planetvel*sin(circrand),0.0\
-	, planetvel*cos(circrand),massrand]);	# initial velocity is orbital
-		velocities.append_array([0.0,0.0,0.0,0.0]) #zero acceleration
-	var mesharray = PackedFloat32Array(interimarray)
+	#var mesharray = PackedFloat32Array(interimarray)
 	print ("planets total mass: ",totalplanetmass)
-	return mesharray
+	#return mesharray
 	#end of test buffer
 
-func makeplanet(interimarray,size,mass,position,velocity):
-	print("nothing")
+func makeplanet(planettransform,velocity,planetindex):
+	RenderingServer.multimesh_instance_set_transform(planet_Multimesh, planetindex, planettransform)
+	# Array 1:3 velocity, 4-mass 5:7 acceleration, 8 free
+	velocities.append_array(velocity) #x,y,z,mass
+	velocities.append_array([0.0,0.0,0.0,0.0]) #zero acceleration
+	if(planetindex>1990):
+		print("instance: ",RenderingServer.multimesh_instance_get_transform(planet_Multimesh, planetindex))
+	
 
 func CreateMultimesh(size):
-	multimeshid = RenderingServer.multimesh_create()
+	var multimeshid = RenderingServer.multimesh_create()
 	RenderingServer.multimesh_allocate_data(\
 	multimeshid,\
 	size,\
@@ -179,13 +167,11 @@ func CreateMultimesh(size):
 	false, false,false) #colours, custom, useindirect
 	
 	RenderingServer.multimesh_set_mesh(multimeshid, TestMesh.mesh.get_rid())
-	RenderingServer.multimesh_set_buffer(multimeshid,tempbuffer)
+	#RenderingServer.multimesh_set_buffer(multimeshid,tempbuffer)
 	var aabb = Vector3(512.0, 1000.0, 512.0)
-	var instance = RenderingServer.instance_create()
 	var scenario =  get_world_3d().scenario
-	#RenderingServer.instance_set_custom_aabb(instance,AABB(-aabb,2*aabb))
-	RenderingServer.instance_set_scenario(instance,scenario)
-	RenderingServer.instance_set_base(instance,multimeshid)
+	var instance = RenderingServer.instance_create2(multimeshid, scenario)
+	RenderingServer.instance_set_custom_aabb(instance,AABB(-aabb,2*aabb))
 	return multimeshid
 	
 #Move to planet_data
@@ -193,15 +179,10 @@ func create_texturearray():
 	# textures from https://www.solarsystemscope.com/textures/
 	var texturearray :Texture2DArray = Texture2DArray.new()
 	var images :Array[Image] = []
-	#var imagelist =["8k_sun.jpg", "8k_mercury.jpg", "8k_venus_surface.jpg", \
-	#"8k_earth_daymap.jpg","8k_mars.jpg","8k_jupiter.jpg", "8k_saturn.jpg", \
-	#"2k_uranus.jpg", "2k_neptune.jpg", "8k_moon.jpg","4k_ceres_fictional.jpg","4k_eris_fictional.jpg" ]
-	
 	var imagelist =["4k_sun.jpg", "4k_mercury.jpg", "4k_venus_surface.jpg", \
 	"4k_earth_daymap.jpg","4k_mars.jpg","4k_jupiter.jpg", "4k_saturn.jpg", \
 	"4k_uranus.jpg", "4k_neptune.jpg", "4k_pluto.jpg", \
 	"4k_moon.jpg","4k_ceres_fictional.jpg","4k_eris_fictional.jpg","4k_makemake_fictional.jpg",  "4k_haumea_fictional.jpg" ]
-	
 	
 	#var imagelist =["2k_sun.jpg","testerror.jpg", "mercury.png", "venus.png", "Earth.jpeg","Mars.jpeg","Jupiter.jpeg", "saturn.jpg", "uranus.jpg","moon.png" ]
 	
@@ -211,6 +192,7 @@ func create_texturearray():
 		if ResourceLoader.exists(file_path):
 			var temptexture = load(file_path)
 			img = temptexture.get_image()
+			#Dont need these steps if all images same size and format
 			#img.decompress()
 			#img.resize(4096,2048)
 			#img.convert(Image.FORMAT_RGB8)
